f6f0cc8767f82ef2ac0bc06dd33a0372e66b5c99
Benjamin M. Brown <benjaminbb@gmail.com>
2016-02-11 00:07:01 -0600

    worker function to create pull and subscriber sockets, when some logic for pulls vs subs

--- zmq-push-server.py
+++ zmq-push-server.py
@@ -19,15 +19,50 @@ def push_server(port="5556"):
 		time.sleep(1)
 
 def publishing_server(port="5558"):
-	
+
 	context = zmq.Context()
 	socket = context.socket(zmq.PUB)
 	socket.bind("tcp://*:%s" % port)
 	publisher_id = random.randrange(0,9999)
 
 	for reqnum in range(100):
-		topic = ranomd.randrange(8,10)
+		topic = random.randrange(8,10)
 		messagedata = "server#%s" % (topic, messagedata)
 		print "%s %s" % (topic, messagedata)
 		socket.send("%d %s" % (topic, messagedata))
 		time.sleep(1)
+
+def worker(port_push, port_sub):
+	context = zmq.Context()
+	socket_pull = context.socket(zmq.PULL)
+	socket_pull.connect("tcp://localhost:%s" % port_push)
+
+	print "Connected to server with port %s" % port_push
+
+	socket_sub = context.socket(zmq.SUB)
+	socket_sub = connect("tcp://localhost:%s" %port_sub)
+	socket_sub.setsockopt(zmq.SUBSCRIBE, "9") #only subscribe to 9 topics
+
+	#setup zmq poller to poll for messages on the socket connection to both
+	#command server and publsher
+
+	poller = zmq.Poller()
+	poller.register(socket_pull, zmq.POLLIN)
+	poller.register(socket_sub, zmq.POLLIN)
+
+	#now we are doing work
+	sould_continue = True
+	while should_continue:
+		socks = dict(poller.poll())
+		if socket_pull in socks and socks[socket_pull] == zmq.POLLIN:
+			message = socket_pull.recv()
+			print "Received a control command : %s" % message
+			if message == "Exit":
+				print "Got an exit command, client will stop receiving messages"
+				should_continue = False
+
+		if socket_sub in socks and socks[socket_sub] == zmq.POLLIN:
+			string = socket_sub.recv()
+			topic,messagedata = string.split()
+			print "Processing ... ", topic, messagedata
+			
